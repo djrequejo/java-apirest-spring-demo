8db040529979d72c49dff35b06b42a536c3065cc
Diego Requejo Torero <djrequejo@gmail.com>
2024-02-09 08:09:51 -0500

    Initial project

---
+++ src/main/java/com/djrequejo/interview/apiuserposts/ApiUserPostsApplication.java
@@ -0,0 +1,13 @@
+package com.djrequejo.interview.apiuserposts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUserPostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djrequejo/interview/apiuserposts/ApiUserPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djrequejo.interview.apiuserposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserPostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
